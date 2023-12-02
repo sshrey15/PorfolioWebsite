73082f6e9fa09b14908f3620f86b5beff0566912
shrey <shreysingh152004@gmail.com>
2023-12-02 19:17:13 +0530

    Update image sources and textures

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('shrey.jpeg');
+const jeffTexture = new THREE.TextureLoader().load('jeff.png');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
